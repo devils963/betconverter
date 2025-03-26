144d2875a5ddd62b3be9b30b46c636c2ed454409
devils963 <lateefnns@outlook.com>
2025-03-26 08:55:13 +0000

    Update ESLint configuration to fix TypeScript linting issues

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -36,7 +36,9 @@ const config = {
           "attributes": false
         }
       }
-    ]
+    ],
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-empty-object-type": "off"
   }
 }
 module.exports = config;
